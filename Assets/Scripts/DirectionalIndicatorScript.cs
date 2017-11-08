b9e3b07cc0ec85760aa12353ce52538b9dd8dd6b
KaiKheng Lee <khengl97@gmail.com>
2017-11-09 00:56:24 +0800

    Implement directional indicator, reimpliment atk indi, and update timeline

---
+++ Assets/Scripts/DirectionalIndicatorScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalIndicatorScript : MonoBehaviour {
+
+	public GameObject model;
+
+	// Use this for initialization
+	void Start () 
+	{
+		model = gameObject.transform.GetChild(0).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			if(!model.activeInHierarchy)
+			{
+				model.SetActive(true);
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			if(model.activeInHierarchy)
+			{
+				model.SetActive(false);
+			}
+		}
+	}
+}
