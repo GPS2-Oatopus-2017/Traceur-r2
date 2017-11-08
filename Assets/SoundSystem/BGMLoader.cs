47f0cfd90248e68ee30307178f065fe339f99908
Syabil <0114962@kdu-online.com>
2017-11-08 09:47:56 +0800

    Edit script & Add sound
    -Edited my "BGMLoader" script
    -Added few SFXs

--- Assets/SoundSystem/BGMLoader.cs
+++ Assets/SoundSystem/BGMLoader.cs
@@ -5,10 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class BGMLoader : MonoBehaviour
 {
-	public AudioClipID bgm;
-
 	void Start()
 	{
-		SoundManagerScript.Instance.PlayBGM(bgm);
+		if(SceneManager.GetActiveScene().name == "MainMenu")
+		{
+			SoundManagerScript.Instance.PlayBGM(AudioClipID.BGM_MAIN_MENU);
+		}
+
+		if(SceneManager.GetActiveScene().name == "Tutorial")
+		{
+			SoundManagerScript.Instance.StopBGM(AudioClipID.BGM_MAIN_MENU);
+		}
+
+		if(SceneManager.GetActiveScene().name == "WinScene")
+		{
+			SoundManagerScript.Instance.PlayOneShotSFX2D(AudioClipID.BGM_WIN);
+		}
+
+		if(SceneManager.GetActiveScene().name == "LoseScene")
+		{
+			SoundManagerScript.Instance.PlayOneShotSFX2D(AudioClipID.BGM_LOSE);
+		}
 	}
 }
