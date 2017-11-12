c1f54eecb464f1ffe21e98d2c1206f2b9edff4e9
Teo Jia Luen <jialuenteo@gmail.com>
2017-11-13 01:34:48 +0800

    Scoring Half-complete (need to normalize numbers)

---
+++ Assets/Scripts/Player/PlayerScoreScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScoreScript : MonoBehaviour {
+
+	public GameObject curWaypoint,playerCollider, waypointCollider;
+	public Vector3 waypointCenter, playerCenter;
+	public float swipeLocation, playerDirection;
+	public bool startCalculating;
+
+	// Use this for initialization
+	void Start () 
+	{
+		FindPlayer();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (startCalculating)
+		{
+			calculateDistance();
+		}
+
+	}
+
+	void FindPlayer()
+	{
+		playerCollider = gameObject;
+
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.tag == "Waypoint")
+		{
+			curWaypoint = col.gameObject;
+			startCalculating = true;
+		}
+	}
+
+	void calculateDistance()
+	{
+		waypointCenter = curWaypoint.transform.position;
+		playerCenter = gameObject.transform.position;
+		playerDirection = gameObject.GetComponent<PlayerCoreController>().rigidController.rotAngle;
+
+		if(Input.anyKey || Input.touchCount > 0)
+		{
+			if (playerDirection < 10 && playerDirection > 350 || playerDirection > 170 && playerDirection < 190)
+			{
+				swipeLocation = waypointCenter.z - playerCenter.z;
+				startCalculating = false;	
+			}
+			else if (playerDirection < 100 && playerDirection > 80 || playerDirection > 260 && playerDirection < 280)
+			{
+				swipeLocation = waypointCenter.x - playerCenter.x;
+				startCalculating = false;	
+			}
+
+		}
+	}
+}
