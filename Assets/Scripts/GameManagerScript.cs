8746e0c9fae20beffb393bd5abf5dfb8d2f9d919
Eugene <aagene96@gmail.com>
2017-11-19 15:18:07 +0800

    Branch of Eugene

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -27,38 +27,38 @@ public class GameManagerScript : MonoBehaviour
 
 	void Update()
 	{
-		CheckWinLoseConditions();
+//		CheckWinLoseConditions();
 	}
 
-	[Header("WinLoseConditions")]
-	public TimerScript timerScript;
-
-	void CheckWinLoseConditions () 
-	{
-      //  if(PlayerStatusScript.Instance.health > 0) return; //After character health reaches "0" change scene to [LoseScene]
-		if(player.status.currentHealth <= 0)
-        {
-            DialogueManager.Instance.LoseSceneDialogue();
-
-            if(((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || Input.GetMouseButtonDown(0)))
-            {
-                Time.timeScale = 1.0f;
-                GetComponent<ChangeSceneScript>().ChangeScenes(1);
-            }
-        }
-		//else if(timerScript.totalTimeLevel1 > 0) return;// After count-down timer reaches "0" change scene to [LoseScene]
-		//else
-        if(timerScript.totalTimeLevel1 <= 0)
-		{
-			DialogueManager.Instance.LoseSceneDialogue();
-
-			player.status.currentHealth = 0;
-
-			if(((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || Input.GetMouseButtonDown(0)))
-			{
-				Time.timeScale = 1.0f;
-				GetComponent<ChangeSceneScript>().ChangeScenes(1);
-			}
-		}
-	}
+//	[Header("WinLoseConditions")]
+//	public TimerScript timerScript;
+//
+//	void CheckWinLoseConditions () 
+//	{
+//      //  if(PlayerStatusScript.Instance.health > 0) return; //After character health reaches "0" change scene to [LoseScene]
+//		if(player.status.currentHealth <= 0)
+//        {
+//            DialogueManager.Instance.LoseSceneDialogue();
+//
+//            if(((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || Input.GetMouseButtonDown(0)))
+//            {
+//                Time.timeScale = 1.0f;
+//                GetComponent<ChangeSceneScript>().ChangeScenes(1);
+//            }
+//        }
+//		//else if(timerScript.totalTimeLevel1 > 0) return;// After count-down timer reaches "0" change scene to [LoseScene]
+//		//else
+//        if(timerScript.totalTimeLevel1 <= 0)
+//		{
+//			DialogueManager.Instance.LoseSceneDialogue();
+//
+//			player.status.currentHealth = 0;
+//
+//			if(((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || Input.GetMouseButtonDown(0)))
+//			{
+//				Time.timeScale = 1.0f;
+//				GetComponent<ChangeSceneScript>().ChangeScenes(1);
+//			}
+//		}
+//	}
 }
