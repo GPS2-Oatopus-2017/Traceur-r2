8e65ced4a48b6ff95d46a06e53847b15d91fc2ab
FattyMieo <fong2009@live.com.my>
2017-12-05 15:44:38 +0800

    Import new skybox for Level 1 & Fix animation

---
+++ Assets/SkyboxVariator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxVariator : MonoBehaviour
+{
+	private int startRot = 0;
+	private float speed = 0.1f;
+	private bool negator = false;
+	void Start ()
+	{
+		startRot = Random.Range(0, 360);
+		negator = (Random.Range(0, 2) == 0 ? false : true);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		RenderSettings.skybox.SetFloat("_Rotation", startRot + (Time.time * (negator ? -speed : speed)));
+	}
+}
